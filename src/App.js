e8299b34d74ccb73b9f0b3b03180ad2bc2fb1ebd
Paul Dornfeld <pdornfeld@bostonlogic.com>
2017-07-24 15:59:03 -0400

    paul

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Paul Dornfeld</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lets Get Started.
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
         </p>
       </div>
     );
